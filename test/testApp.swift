f09642670721098fd9ace6a54ba946e9acf271bb
mr-koww <mr-koww@yandex.ru>
2021-09-03 18:19:30 +0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by mr-koww on 03.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
